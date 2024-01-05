646c4c351e8590dd42b9a3927fc6bc789bf25b41
Jonathan Perry <jpper@umich.edu>
2024-01-05 13:25:08 -0600

    Upgrade ionic app to most recent, fix android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,10 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { IonRange } from "@ionic/angular"
-import { Howl } from "howler"
+import { IonRange } from '@ionic/angular';
+import { Howl } from 'howler';
 
 export interface Track {
   name: string;
-  path: string
+  path: string;
 }
 
 @Component({
@@ -16,61 +16,61 @@ export class HomePage implements OnInit {
   playlist: Track[];
   loadedPlaylist: Track[];
 
-  activeTrack: Track = null
-  player: Howl = null;
+  activeTrack: Track = null as any;
+  player: Howl = null as any;
   isPlaying = false;
   progress = 0;
-  @ViewChild("range") range: IonRange;
+  @ViewChild('range') range: IonRange;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.loadedPlaylist = [
       {
-        name: "Funny Song",
-        path: "./assets/mp3/bensound-funnysong.mp3"
+        name: 'Funny Song',
+        path: './assets/mp3/bensound-funnysong.mp3',
       },
       {
-        name: "Jazzy Frenchie",
-        path: "./assets/mp3/bensound-jazzyfrenchy.mp3"
+        name: 'Jazzy Frenchie',
+        path: './assets/mp3/bensound-jazzyfrenchy.mp3',
       },
       {
-        name: "Love",
-        path: "./assets/mp3/bensound-love.mp3"
+        name: 'Love',
+        path: './assets/mp3/bensound-love.mp3',
       },
       {
-        name: "A New Beginning",
-        path: "./assets/mp3/bensound-anewbeginning.mp3"
+        name: 'A New Beginning',
+        path: './assets/mp3/bensound-anewbeginning.mp3',
       },
       {
-        name: "Better Days",
-        path: "./assets/mp3/bensound-betterdays.mp3"
+        name: 'Better Days',
+        path: './assets/mp3/bensound-betterdays.mp3',
       },
       {
-        name: "Cute",
-        path: "./assets/mp3/bensound-cute.mp3"
+        name: 'Cute',
+        path: './assets/mp3/bensound-cute.mp3',
       },
       {
-        name: "Hey",
-        path: "./assets/mp3/bensound-hey.mp3"
+        name: 'Hey',
+        path: './assets/mp3/bensound-hey.mp3',
       },
       {
-        name: "Tomorrow",
-        path: "./assets/mp3/bensound-tomorrow.mp3"
+        name: 'Tomorrow',
+        path: './assets/mp3/bensound-tomorrow.mp3',
       },
       {
-        name: "Slow Motion",
-        path: "./assets/mp3/bensound-slowmotion.mp3"
+        name: 'Slow Motion',
+        path: './assets/mp3/bensound-slowmotion.mp3',
       },
       {
-        name: "Moose",
-        path: "./assets/mp3/bensound-moose.mp3"
+        name: 'Moose',
+        path: './assets/mp3/bensound-moose.mp3',
       },
       {
-        name: "Adventure",
-        path: "./assets/mp3/bensound-adventure.mp3"
-      }
-    ]
+        name: 'Adventure',
+        path: './assets/mp3/bensound-adventure.mp3',
+      },
+    ];
     this.initializeSongs();
   }
 
@@ -84,16 +84,14 @@ export class HomePage implements OnInit {
       onplay: () => {
         this.isPlaying = true;
         this.activeTrack = track;
-        this.updateProgress()
+        this.updateProgress();
       },
-      onend: () => {
-
-      }
-    })
-    this.player.play()
+      onend: () => {},
+    });
+    this.player.play();
   }
 
-  togglePlayer(pause) {
+  togglePlayer(pause: any) {
     this.isPlaying = !pause;
     if (pause) {
       this.player.pause();
@@ -112,11 +110,11 @@ export class HomePage implements OnInit {
   }
 
   prev() {
-    let index = this.playlist.indexOf(this.activeTrack)
+    let index = this.playlist.indexOf(this.activeTrack);
     if (index > 0) {
-      this.start(this.playlist[index - 1])
+      this.start(this.playlist[index - 1]);
     } else {
-      this.start(this.playlist[this.playlist.length - 1])
+      this.start(this.playlist[this.playlist.length - 1]);
     }
   }
 
@@ -131,27 +129,27 @@ export class HomePage implements OnInit {
     this.progress = (seek / this.player.duration()) * 100 || 0;
     setTimeout(() => {
       this.updateProgress();
-    }, 1000)
+    }, 1000);
   }
 
   initializeSongs() {
     this.playlist = this.loadedPlaylist;
   }
 
-  filterSongs(event) {
+  filterSongs(event: any) {
     this.initializeSongs();
     const val = event.target.value;
     if (!val) {
       return;
     }
-    this.playlist = this.playlist.filter(currentSong => {
+    this.playlist = this.playlist.filter((currentSong) => {
       if (currentSong.name && val) {
         if (currentSong.name.toLowerCase().indexOf(val.toLowerCase()) > -1) {
           return true;
         }
         return false;
       }
+      return false
     });
   }
-
 }
